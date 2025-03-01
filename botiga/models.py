ce269ea32a004cb770abefbc7243957a4428a7e9
ToniMendza <antmendogar@gmail.com>
2025-03-01 13:23:19 +0100

    Primera taula

--- botiga/models.py
+++ botiga/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+import re
+from django.core.exceptions import ValidationError
 
-# Create your models here.
-class IVA(models.Model):
-    name = models.CharField(max_length=200)
+def validate_email(value):
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if not re.match(email_regex, value):
+        raise ValidationError("El email no es valid")
 
-class factura(models.Model):
-    iva_id = models.ForeignKey(IVA, on_delete=models.CASCADE)
+class Usuari(models.Model):
+    nom_usu = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=255, unique=True, validators=[validate_email])
+    contrasenya = models.CharField(max_length=255, null=False)
+    adreca_usu = models.CharField(max_length=255, null=True)
